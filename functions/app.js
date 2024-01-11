3d0a6d4581215cf417ba088c8352affaddd5a533
Virendra Jadeja <122339909+virendra-bytes@users.noreply.github.com>
2024-01-11 18:12:16 +0530

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -17,37 +17,31 @@ app.post("/chargeCard", async (request, response) => {
   // step2: charge the card.
   try {
     console.log("Inside the cardCharges method", request.body);
-
-    const token = await stripe.tokens.create({
-      card: {
-        number: request.body.cardNumber,
-        exp_month: request.body.cardExpMonth,
-        exp_year: request.body.cardExpYear,
-        cvc: request.body.cvv,
-      },
-    });
-
-    // should be removed on production
-    console.log("token => ",token.id);
-
-    const charge = await stripe.charges.create({
+    const paymentIntent = await stripe.paymentIntents.create({
       amount: request.body.amount * 100,
       currency: 'usd',
-      description: request.body.description,
-      source: token.id,
-      receipt_email: request.body.email
+      customer: request.body.cusotmer_id,
+      payment_method: request.body.payment_method_id,
+      confirm: true,
+      description: request.body.description
     });
-
-    // should be removed on production
-    console.log("charge => ",{"receipt":charge.receipt_url,"status": charge.status,"paid":charge.paid});
-
-    return response.send({ success: true });
+    console.log("Completed the stripe method invocation", JSON.stringify(paymentIntent));
+    return response.send(generateResponse(paymentIntent));
 
   } catch (e) {
-    // Display error on client
     return response.send({ error: e.message });
   }
 });
+const generateResponse = (intent) => {
+  if (intent.status === "succeeded") {
+    return {
+      success: true,
+    };
+  } else {
+    return {
+      error: "Invalid PaymentIntent status",
+    };
+  }
+};
 
-// request handlers
 module.exports = app;
